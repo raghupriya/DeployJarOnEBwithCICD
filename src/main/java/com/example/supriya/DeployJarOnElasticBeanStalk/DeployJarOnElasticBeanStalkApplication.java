ce179190b0744ab982859e1813b065d26c21dc84
raghupriya <supriya.ramchandra@gmail.com>
2020-04-01 15:42:55 -0700

    creating the project

---
+++ src/main/java/com/example/supriya/DeployJarOnElasticBeanStalk/DeployJarOnElasticBeanStalkApplication.java
@@ -0,0 +1,13 @@
+package com.example.supriya.DeployJarOnElasticBeanStalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployJarOnElasticBeanStalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployJarOnElasticBeanStalkApplication.class, args);
+	}
+
+}
